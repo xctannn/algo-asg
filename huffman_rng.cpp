95be17cf305f78f5138c6f523cec5ec216837b2b
Ryan-Then <48986079+Ryan-Then@users.noreply.github.com>
2023-06-03 11:50:06 +0800

    Add files via upload
    Version 0.1.2

--- huffman_rng.cpp
+++ huffman_rng.cpp
@@ -4,11 +4,8 @@
 #include <random>
 #include <set>
 
-std::string generateRandomWord(std::mt19937& rng, int wordLength) {
-    static const char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    static const int alphabetSize = sizeof(alphabet) - 1;
-
-    std::uniform_int_distribution<int> dist(0, alphabetSize - 1);
+std::string generateRandomWord(std::mt19937& rng, int wordLength, const std::string& alphabet) {
+    std::uniform_int_distribution<int> dist(0, alphabet.length() - 1);
 
     std::string word;
     for (int i = 0; i < wordLength; ++i) {
@@ -18,7 +15,7 @@ std::string generateRandomWord(std::mt19937& rng, int wordLength) {
     return word;
 }
 
-void generateRandomWords(int numWords, int wordLength, const std::string& filename) {
+void generateRandomWords(int numWords, int wordLength, const std::string& alphabet, const std::string& filename) {
     std::ofstream outputFile(filename);
     if (!outputFile) {
         std::cerr << "Error opening output file." << std::endl;
@@ -28,15 +25,21 @@ void generateRandomWords(int numWords, int wordLength, const std::string& filena
     std::random_device rd;
     std::mt19937 rng(rd());
 
+    std::string outputString;
     std::set<char> uniqueChars;
 
     for (int i = 0; i < numWords; ++i) {
-        std::string word = generateRandomWord(rng, wordLength);
+        std::string word = generateRandomWord(rng, wordLength, alphabet);
 
         // Collect unique characters
         for (char c : word) {
-            uniqueChars.insert(c);
+            if (uniqueChars.find(c) == uniqueChars.end()) {
+                uniqueChars.insert(c);
+            }
         }
+
+        // Write generated word to file
+        outputString += word + " ";
     }
 
     // Write unique characters count
@@ -48,9 +51,7 @@ void generateRandomWords(int numWords, int wordLength, const std::string& filena
     }
 
     // Write generated words in one line separated by spaces
-    for (int i = 0; i < numWords; ++i) {
-        outputFile << generateRandomWord(rng, wordLength) << " ";
-    }
+    outputFile << outputString << std::endl;
 
     outputFile.close();
     std::cout << "Random words generated successfully and saved to " << filename << std::endl;
@@ -67,13 +68,18 @@ int main() {
     std::cout << "Enter the length of each word: ";
     std::cin >> wordLength;
 
-    // Generate random words and save to a file
+    // Get random set of characters from the user
+    std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+    // Generate random words and save to a file with dynamic filename
     std::string filename = "huffmancoding_";
     std::string paddedNumWords = std::to_string(numWords);
     paddedNumWords = std::string(8 - paddedNumWords.length(), '0') + paddedNumWords;
     filename += paddedNumWords + "_input.txt";
 
-    generateRandomWords(numWords, wordLength, filename);
+    generateRandomWords(numWords, wordLength, alphabet, filename);
 
     return 0;
 }
+
